2c576e66855cdc9e7d074391a07eb4e50aedbf59
JoeRamage <2014001424@student.sit.ac.nz>
2016-10-07 14:58:19 +1300

    AddedFolder

---
+++ BootLoot-master/Assets/Scripts/playerResets.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerResets : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject kickCollider;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void KickReset() {
+		GameObject.Find ("controllerCollider").GetComponent<Player> ().kicking = false;
+	}
+
+	public void kickOn() {
+		kickCollider.SetActive (true);
+	}
+
+	public void kickOff() {
+		kickCollider.SetActive (false);
+	}
+}
