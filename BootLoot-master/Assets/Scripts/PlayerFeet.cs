dd4174aac2058b5d40a7b1e47f421fb1129e7847
Joe Simmonds <joe.simmonds@sit.ac.nz>
2016-10-12 13:55:40 +1300

    added player feet scripts
    added player feet scripts and terrain tags

---
+++ BootLoot-master/Assets/Scripts/PlayerFeet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFeet : MonoBehaviour {
+
+	[SerializeField]
+	private float terrainType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if(other.tag == "tWater") {
+			terrainType = 0.50f;
+		}
+		if(other.tag == "tGrass") {
+			terrainType = 1.50f;
+		}
+		if(other.tag == "tWood") {
+			terrainType = 2.50f;
+		}
+		if(other.tag == "tDirt") {
+			terrainType = 3.50f;
+		}
+	}
+}
