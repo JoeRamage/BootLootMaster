769bff6ebf3bd6fbd7c5527f6c0cbe543b877097
Joe Simmonds <joe.simmonds@sit.ac.nz>
2016-10-13 16:08:17 +1300

    added house icons
    added house icons and cleaned up the hierarchy a bit

---
+++ BootLoot-master/Assets/Scripts/jackie/jackieDoor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class jackieDoor : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject indicator; 
+	public bool overJackieDoor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Player") {
+			indicator.SetActive (true);
+			overJackieDoor = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+		if(other.tag == "Player") {
+			indicator.SetActive (false);
+			overJackieDoor = false;
+		}
+	}
+}
---
+++ BootLoot-master/Assets/Scripts/oslo/osloDoor.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class osloDoor : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject indicator; 
+	public bool overOsloDoor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.tag == "Player") {
+			indicator.SetActive (true);
+			overOsloDoor = true;
+		}
+	}
+	void OnTriggerExit2D (Collider2D other) {
+		if(other.tag == "Player") {
+			indicator.SetActive (false);
+			overOsloDoor = false;
+		}
+	}
+}
